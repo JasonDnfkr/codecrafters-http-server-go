395fe770af22072397827d9d24132f3a6a5377e7
JasonDnfkr <407405010@qq.com>
2024-06-07 16:20:23 +0800

    respond with body

--- app/server.go
+++ app/server.go
@@ -46,99 +46,6 @@ func Response(conn net.Conn) {
 	}
 }
 
-func ExtractUrlPath(conn net.Conn) {
-	buffer := make([]byte, 1024)
-	_, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading from connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	bufferStr := string(buffer)
-
-	for i := 0; i < len(bufferStr); i++ {
-		if bufferStr[i] == '/' {
-			idx := i + 1
-			for idx < len(bufferStr) && bufferStr[idx] == ' ' {
-				idx++
-			}
-			if bufferStr[idx:idx+4] == "HTTP" {
-				_, err := conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-				fmt.Printf("Response 200 Completed")
-				if err != nil {
-					fmt.Println("Error writing to connection: ", err.Error())
-					os.Exit(1)
-				}
-				break
-			} else {
-				_, err := conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-				if err != nil {
-					fmt.Println("Error writing to connection: ", err.Error())
-					os.Exit(1)
-				}
-			}
-		}
-	}
-}
-
-func RespondWithBody(conn net.Conn) {
-	buffer := make([]byte, 1024)
-	_, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading from connection: ", err.Error())
-		return
-	}
-
-	lines := strings.Split(string(buffer), CRLF)
-	for i, line := range lines {
-		fmt.Printf("Line %d: %s\n", i+1, line)
-	}
-	fmt.Println("========================")
-
-	path := strings.Split(lines[0], " ")[1]
-	fmt.Println("path: " + path)
-	words := strings.Split(path, "/")
-
-	var respStr string
-	var found = false
-
-	for i, word := range words {
-		if word == "echo" {
-			respStr = words[i+1]
-			found = true
-			break
-		}
-	}
-
-	if !found {
-		_, err := conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		if err != nil {
-			fmt.Println("Error writing to connection: ", err.Error())
-			os.Exit(1)
-		}
-	}
-
-	var resp strings.Builder
-	resp.WriteString("HTTP/1.1 200 OK")
-	resp.WriteString(CRLF)
-	resp.WriteString("Content-Type: text/plain")
-	resp.WriteString(CRLF)
-	resp.WriteString("Content-Length: " + strconv.Itoa(len(respStr)))
-	resp.WriteString(CRLF + CRLF)
-	resp.WriteString(respStr)
-
-	fmt.Println("============= response =============")
-	fmt.Println(resp.String())
-	fmt.Println("============= response end =============")
-
-	_, err = conn.Write([]byte(resp.String()))
-	fmt.Printf("echo: %s\n", respStr)
-	if err != nil {
-		fmt.Println("Error writing to connection: ", err.Error())
-		return
-	}
-}
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -157,6 +64,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	//ExtractUrlPath(conn)
 	Response(conn)
 }
