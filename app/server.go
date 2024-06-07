58b297dca1e188e9a3dc6f6637753a5565f92b23
JasonDnfkr <407405010@qq.com>
2024-06-07 18:42:46 +0800

    reconstruct

--- app/server.go
+++ app/server.go
@@ -48,65 +48,67 @@ func ResponseHandler(conn net.Conn) {
 		return fmt.Sprintf("%s%s%s%s%s", statusLine, CRLF, header, CRLF, body)
 	}
 
-	buffer := make([]byte, 1024)
-	_, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading from connection: ", err.Error())
-		return
-	}
-
-	request := string(buffer)
-
-	var statusLine string
-	var headers map[string]string
-	var body string
-
-	lines := strings.Split(request, CRLF)
-	fmt.Println("======= HTTP REQUEST =======")
-	for _, line := range lines {
-		fmt.Println(line)
-	}
-	fmt.Println("======= HTTP REQUEST END =======")
-
-	statusLine = createStatusLine(true)
-	addHeaders("Content-Type", "text/plain", &headers)
-	addHeaders("Content-Length", "0", &headers)
-
-	path := strings.Split(lines[0], " ")[1]
-	if path == "/" {
-		statusLine = createStatusLine(true)
-		conn.Write([]byte(statusLine + CRLF + CRLF))
-
-		return
-	} else if strings.Split(path, "/")[1] == "echo" {
-		body = strings.Split(path, "/")[2]
-		addHeaders("Content-Length", strconv.Itoa(len(body)), &headers)
-	} else if strings.Split(path, "/")[1] == "user-agent" {
-		fmt.Println("get user agent header")
-		for _, line := range lines {
-			if strings.HasPrefix(line, "User-Agent:") {
-				// get foobar/1.2.3 ...
-				content := strings.Split(line, " ")[1]
-				addHeaders("Content-Length", strconv.Itoa(len(body)), &headers)
-				body = content
-			}
+	for {
+		buffer := make([]byte, 1024)
+		_, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			os.Exit(1)
 		}
-	} else {
-		statusLine = createStatusLine(false)
-		headers = make(map[string]string)
-		body = ""
-		conn.Write([]byte(statusLine + CRLF + CRLF))
 
-		return
+		request := string(buffer)
+
+		var statusLine string
+		var headers map[string]string
+		var body string
+
+		lines := strings.Split(request, CRLF)
+		fmt.Println("======= HTTP REQUEST =======")
+		for _, line := range lines {
+			fmt.Println(line)
+		}
+		fmt.Println("======= HTTP REQUEST END =======")
+
+		statusLine = createStatusLine(true)
+		addHeaders("Content-Type", "text/plain", &headers)
+		addHeaders("Content-Length", "0", &headers)
+
+		path := strings.Split(lines[0], " ")[1]
+		if path == "/" {
+			statusLine = createStatusLine(true)
+			conn.Write([]byte(statusLine + CRLF + CRLF))
+
+			return
+		} else if strings.Split(path, "/")[1] == "echo" {
+			body = strings.Split(path, "/")[2]
+			addHeaders("Content-Length", strconv.Itoa(len(body)), &headers)
+		} else if strings.Split(path, "/")[1] == "user-agent" {
+			fmt.Println("get user agent header")
+			for _, line := range lines {
+				if strings.HasPrefix(line, "User-Agent:") {
+					// get foobar/1.2.3 ...
+					content := strings.Split(line, " ")[1]
+					addHeaders("Content-Length", strconv.Itoa(len(body)), &headers)
+					body = content
+				}
+			}
+		} else {
+			statusLine = createStatusLine(false)
+			headers = make(map[string]string)
+			body = ""
+			conn.Write([]byte(statusLine + CRLF + CRLF))
+
+			return
+		}
+
+		resp := createHttpResponse(statusLine, buildHeader(headers), body)
+
+		fmt.Println("======== RESPONSE ========")
+		fmt.Println(resp)
+		fmt.Println("======== RESPONSE END =======")
+
+		conn.Write([]byte(resp))
 	}
-
-	resp := createHttpResponse(statusLine, buildHeader(headers), body)
-
-	fmt.Println("======== RESPONSE ========")
-	fmt.Println(resp)
-	fmt.Println("======== RESPONSE END =======")
-
-	conn.Write([]byte(resp))
 }
 
 func Response(conn net.Conn) {
@@ -176,11 +178,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	ResponseHandler(conn)
+		go ResponseHandler(conn)
+	}
 }
