99c4e4def0471bb8fadc7db1ef78bc81d5fca91a
JasonDnfkr <407405010@qq.com>
2024-06-07 15:06:17 +0800

    extract url path

--- app/server.go
+++ app/server.go
@@ -7,36 +7,15 @@ import (
 	"os"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
+func ExtractUrlPath(conn net.Conn) {
 	buffer := make([]byte, 1024)
-	_, err = conn.Read(buffer)
+	_, err := conn.Read(buffer)
 	if err != nil {
 		fmt.Println("Error reading from connection: ", err.Error())
 		os.Exit(1)
 	}
 
 	bufferStr := string(buffer)
-
-	//fmt.Println("=============================")
-	//fmt.Println(bufferStr)
-	//fmt.Println("=============================")
 
 	for i := 0; i < len(bufferStr); i++ {
 		if bufferStr[i] == '/' {
@@ -61,4 +40,25 @@ func main() {
 			}
 		}
 	}
+}
+
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+
+	// Uncomment this block to pass the first stage
+	//
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	ExtractUrlPath(conn)
 }
