0cf7b73f1d21b90e90d723616fda900c3f7564a5
JasonDnfkr <407405010@qq.com>
2024-06-08 01:27:52 +0800

    gzip compression

--- app/server.go
+++ app/server.go
@@ -147,6 +147,8 @@ func ResponseHandler(conn net.Conn) {
 					os.Exit(1)
 				}
 
+				fmt.Println("----------------------===-" + buf.String())
+
 				body = requestBody
 				addHeaders("Content-Length", strconv.Itoa(len(buf.String())), &headers)
 			} else {
