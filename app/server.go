a94b887526d8ce71d54f1218ef7057db228c8782
JasonDnfkr <407405010@qq.com>
2024-06-07 16:19:51 +0800

    respond with body

--- app/server.go
+++ app/server.go
@@ -10,6 +10,42 @@ import (
 	"os"
 )
 
+const CRLF = "\r\n"
+
+func Response(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+		return
+	}
+
+	bufferString := string(buffer)
+	fmt.Println(bufferString)
+
+	path := strings.Split(bufferString, " ")[1]
+	fmt.Println("[Path]" + path)
+	if path == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if strings.Split(path, "/")[1] == "echo" {
+		str := strings.Split(path, "/")[2]
+		var resp strings.Builder
+		resp.WriteString("HTTP/1.1 200 OK")
+		resp.WriteString(CRLF)
+		resp.WriteString("Content-Type: text/plain")
+		resp.WriteString(CRLF)
+		resp.WriteString("Content-Length: " + strconv.Itoa(len(str)))
+		resp.WriteString(CRLF + CRLF)
+		resp.WriteString(str)
+
+		conn.Write([]byte(resp.String()))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+}
+
 func ExtractUrlPath(conn net.Conn) {
 	buffer := make([]byte, 1024)
 	_, err := conn.Read(buffer)
@@ -52,8 +88,6 @@ func RespondWithBody(conn net.Conn) {
 		fmt.Println("Error reading from connection: ", err.Error())
 		return
 	}
-
-	const CRLF = "\r\n"
 
 	lines := strings.Split(string(buffer), CRLF)
 	for i, line := range lines {
@@ -124,5 +158,5 @@ func main() {
 	}
 
 	//ExtractUrlPath(conn)
-	RespondWithBody(conn)
+	Response(conn)
 }
