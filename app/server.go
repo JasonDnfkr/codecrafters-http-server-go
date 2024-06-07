66a1ec8fe40450e1a5a24af385533c46783aeb77
JasonDnfkr <407405010@qq.com>
2024-06-07 18:15:35 +0800

    reconstruct

--- app/server.go
+++ app/server.go
@@ -11,6 +11,92 @@ import (
 )
 
 const CRLF = "\r\n"
+const RESPONSE_OK = "HTTP/1.1 200 OK"
+const RESPONSE_FAILED = "HTTP/1.1 404 Not Found"
+
+func ResponseHandler(conn net.Conn) {
+	defer conn.Close()
+
+	createStatusLine := func(isSuccess bool) string {
+		if isSuccess {
+			return RESPONSE_OK
+		} else {
+			return RESPONSE_FAILED
+		}
+	}
+
+	addHeaders := func(headerType string, headerValue string, header *map[string]string) {
+		if *header == nil {
+			*header = make(map[string]string)
+		}
+		(*header)[headerType] = headerValue
+	}
+
+	buildHeader := func(header map[string]string) string {
+		var resp strings.Builder
+		for key, value := range header {
+			resp.WriteString(fmt.Sprintf("%s: %s%s", key, value, CRLF))
+		}
+
+		return resp.String()
+	}
+
+	createHttpResponse := func(statusLine, header, body string) string {
+		return fmt.Sprintf("%s%s%s%s%s", statusLine, CRLF+CRLF, header, CRLF, body)
+	}
+
+	buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+		return
+	}
+
+	request := string(buffer)
+
+	var statusLine string
+	var headers map[string]string
+	var body string
+
+	lines := strings.Split(request, CRLF)
+	fmt.Println("======= HTTP REQUEST =======")
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+	fmt.Println("======= HTTP REQUEST END =======")
+
+	statusLine = createStatusLine(true)
+	addHeaders("Content-Type", "text/plain", &headers)
+	addHeaders("Content-Length", "0", &headers)
+
+	path := strings.Split(lines[0], " ")[1]
+	if path == "/" {
+		statusLine = createStatusLine(true)
+	} else if strings.Split(path, "/")[1] == "echo" {
+		body = strings.Split(path, "/")[2]
+		addHeaders("Content-Length", strconv.Itoa(len(body)), &headers)
+	} else if strings.Split(path, "/")[1] == "user-agent" {
+		fmt.Println("get user agent header")
+		for _, line := range lines {
+			if strings.HasPrefix(line, "User-Agent:") {
+				// get foobar/1.2.3 ...
+				content := strings.Split(line, " ")[1]
+				addHeaders("Content-Length", strconv.Itoa(len(body)), &headers)
+				body = content
+			}
+		}
+	} else {
+		statusLine = createStatusLine(false)
+	}
+
+	resp := createHttpResponse(statusLine, buildHeader(headers), body)
+
+	fmt.Println("======== RESPONSE ========")
+	fmt.Println(resp)
+	fmt.Println("======== RESPONSE END =======")
+
+	conn.Write([]byte(resp))
+}
 
 func Response(conn net.Conn) {
 	defer conn.Close()
@@ -85,5 +171,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	Response(conn)
+	ResponseHandler(conn)
 }
